4775db3e518cdff760cc18b02b1907e6b46768cc
Gabriele <gabrieletuozzo@gmail.com>
2025-06-26 12:03:38 +0200

    added function to convert LLM response to set class

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,30 @@
 def convert_response_to_set(response):
     """
-    Convert the response from watchman query to a set of file paths.
+    Convert the response from LLM.
     """
+    if response == 'idk':
+        return []
     response = response.split(',')
     response = [item.strip() for item in response if item.strip()]
     
     return response
+
+def convert_response_to_set_es(response):
+    """
+    Convert the response from LLM.
+    """
+    if response == 'no sé':
+        return []
+    response = response.split(',')
+    response = [item.strip() for item in response if item.strip()]
+    
+    return response
+
+def convert_response_to_set_class(response,real_classification):
+    """
+    Convert the response from LLM.
+    """
+    if response.lower().strip() != real_classification.lower().strip():
+        return 0
+    else: 
+        return 1
