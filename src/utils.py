657fea49e55f83cdd2884a948afccf75539ea478
Gabriele <gabrieletuozzo@gmail.com>
2025-06-28 13:42:25 +0000

    added results for the experiments in which we try to correct the llms responses

--- src/utils.py
+++ src/utils.py
@@ -28,3 +28,26 @@ def convert_response_to_set_class(response,real_classification):
         return 0
     else: 
         return 1
+    
+def jaccard_similarity(list1, list2):
+    """Calculate the Jaccard similarity between two sets."""
+    set1 = set(list1)
+    set2 = set(list2)
+    intersection = len(set1.intersection(set2))
+    union = len(set1.union(set2))
+    if union == 0:
+        return 0.0
+    return intersection / union
+
+def is_subset(set1, set2):
+    """Check if set1 is a subset of set2."""
+    set1 = set(set1)
+    set2 = set(set2)
+    return set1.issubset(set2)
+
+def is_minus(set1,set2,set3):
+    """Check if set1 minus set2 equals set3."""
+    set1 = set(set1)
+    set2 = set(set2)
+    set3 = set(set3)
+    return set1.difference(set2) == set3
