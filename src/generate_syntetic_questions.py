f75e8983110379ec0329d61f54460767c7a5c254
Gabriele <gabrieletuozzo@gmail.com>
2025-07-12 10:06:54 +0000

    feat: update question generation prompts to use dynamic question count and add minus prompt functionality

--- src/generate_syntetic_questions.py
+++ src/generate_syntetic_questions.py
@@ -6,18 +6,24 @@ import utils
 import json
 
 here = os.path.dirname(os.path.abspath(__file__))
-
 PROMPTS = {
-    "en" : { "equivalence" : '''Generate 10 pairs of diverse questions about different topics, every pairs of questions must be semantically equivalent \
+    "en" : { "equivalence" : '''Generate {number_of_questions_to_generate} pairs of diverse questions about different topics, every pairs of questions must be semantically equivalent \
                         "and the answer to every questions that you formulate must be a list of values, not an ordered list, not a paragraph of text, not a boolean value and not a single number.
                         This is an example of a possible pair of questions: 1. How many regions of France are there? | How many regions does France have?
                         Follow the following format to return the questions: 1. Question1 | Equivalent_Question1
                         Do not add any other kind of text except questions
                         ''',
-            "subset-superset" : '''Generate 10 pairs of diverse questions about different topics, in every pair of questions, the first question must be broader than the second i.e. the answer of the second question must be a subset of the answer of the first \
+            "subset-superset" : '''Generate {number_of_questions_to_generate} pairs of diverse questions about different topics, in every pair of questions, the first question must be broader than the second i.e. the answer of the second question must be a subset of the answer of the first \
                                 and the answer to every questions that you formulate must be a list of values, not an ordered list, not a paragraph of text, not a boolean value and not a single number. \
-                                This is an example of a possible pair of questions: 1. What countries are in the EU? | What countries are in the western EU?''',
-            "minus": '''I prompt to you two questions: question A and question B. A containment relationship hold between the answer of these question, specifically, the responses at the question B is contained in the responses at the question A.\
+                                This is an example of a possible pair of questions: 1. What countries are in the EU? | What countries are in the western EU?
+                                Follow the following format to return the questions: 1. Broader_Question | Subset_Question
+                                Do not add any other kind of text except questions
+                                '''
+    }
+}
+
+PROMPTS_MINUS = {
+    "en" : '''I prompt to you two questions: question A and question B. A containment relationship hold between the answer of these question, specifically, the responses at the question B is contained in the responses at the question A.\
                 Your task now it to generate a question C in a way in which the relationship between the resonses at the three qustions is A-B=C. 
                 The answer to the question C must be a list of values, not an ordered list, not a paragraph of text, not a boolean value and not a single number.
                 This is an example: question A) Which movies star Uma Thurman? question B) Which science fiction movies star Uma Thurman? question C) Which movies star Uma Thurman excluding those science fiction movies star Uma Thurman?
@@ -26,8 +32,7 @@ PROMPTS = {
                 Question A: {q1}
                 Question B: {q2}
                 '''
-    }
-}
+}   
 
 languages = ["en"]
 llm_model = "gemini-2.5-pro"
@@ -36,6 +41,7 @@ output_map = {
     'subset-superset' : 'sup-sub_syntetic.tsv',
     'minus' : 'minus_syntetic.tsv'
 }
+number_of_questions_to_generate = 20
 
 def generate_syntetic_questions():
 
@@ -45,7 +51,7 @@ def generate_syntetic_questions():
             tsv_output = os.path.join(here, f'../data/Dataset/{language}/{output_map[key]}')
             prompt = PromptTemplate(
                 input_variables=["question"],
-                template=PROMPTS[language][key]
+                template=PROMPTS[language][key].format(number_of_questions_to_generate=number_of_questions_to_generate)
             )
             llms = PromptLLMS(prompt, PROMPTS[language][key])
             llm_response = (
@@ -60,9 +66,9 @@ def generate_syntetic_questions():
 def generate_syntetic_questions_minus():
 
     for language in languages:
-        tsv_file = os.path.join(here, f'../data/Dataset/{language}/minus_syntetic.tsv')
+        tsv_file_input = os.path.join(here, f'../data/Dataset/{language}/sup-sub_syntetic.tsv')
         question_pairs = []
-        with open(tsv_file, newline='', encoding='utf-8') as tsvfile:
+        with open(tsv_file_input, newline='', encoding='utf-8') as tsvfile:
             reader = csv.DictReader(tsvfile, delimiter='\t')
             for row in reader:
                 question_pairs.append((row['ql1'], row['ql2']))
@@ -70,7 +76,7 @@ def generate_syntetic_questions_minus():
         for index, (q1, q2) in enumerate(question_pairs):
             prompt_template = PromptTemplate(
             input_variables=["q1", "q2"],
-            template=PROMPTS[language]['minus']
+            template=PROMPTS_MINUS[language]
             )
             llms = PromptLLMS(prompt_template, question1=q2,question2=q1)
 
@@ -80,7 +86,7 @@ def generate_syntetic_questions_minus():
                 else llms.execute_on_gemini_two_question(openAI_model=llm_model)
             )
             parsed = utils.parse_questions_for_tsv(llm_response, input_q1=q1, input_q2=q2, minus=True)
-            utils.save_to_tsv(parsed, tsv_file, minus=True)
+            utils.save_to_tsv(parsed, output_map['minus'], minus=True)
 
 
 generate_syntetic_questions()
