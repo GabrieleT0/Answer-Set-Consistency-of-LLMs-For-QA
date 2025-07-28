c1bead8e5d879edaa95a3887bae70eed5c1b960d
Gabriele <gabrieletuozzo@gmail.com>
2025-07-28 09:40:01 +0000

    Refactor relation classification tests to use dynamic datasets and streamline question handling

--- src/relation_classification.py
+++ src/relation_classification.py
@@ -96,57 +96,48 @@ PROMPTS_DIRECTION = {
     "es" : ''' Ahora Responde solo 'a' o 'b': a) los resultados de q1 están contenidos en q2 b) los resultados de q2 están contenidos en q1. '''
 }
 
-llm_models = ['gpt-4.1-nano-2025-04-14', 'gpt-4.1-mini-2025-04-14','gemini-2.5-pro']
-languages = ['en', 'es']
-real_relations_en = {'equal-wiki.tsv' : 'Equivalence', 'subsetOf-wiki.tsv': 'Containment', 'minus-set.tsv': 'Minus'}
-real_relations_es = {'equal-wiki.tsv' : 'Equivalencia', 'subsetOf-wiki.tsv': 'Contención', 'minus-set.tsv': 'Resta'}
+llm_models = ['deepseek-chat']
+languages = ['en']
 
-def minus_test(llm_model, language):
+def minus_test(llm_model, language, dataset, real_relation):
     """
     Run the minus test for the given LLM model and language.
     """
-    input_filename = 'minus-set.tsv'
-    tsv_file = utils.get_dataset_path(input_filename, language)
+    tsv_file = utils.get_dataset_path(dataset, language)
     prompt = PROMPTS_MINUS[language]
 
     # Load question triples
     question_pairs = []
     with open(tsv_file, newline='', encoding='utf-8') as tsvfile:
-        if language == 'en':
-            reader = csv.DictReader(tsvfile, delimiter=';')
-        else:
-            reader = csv.DictReader(tsvfile, delimiter='\t')
+        reader = csv.DictReader(tsvfile, delimiter='\t')
         for row in reader:
-            question_pairs.append((row['ql1'], row['ql2'], row['ql3']))
+            question_pairs.append((row['Q1'], row['Q3'], row['Q4']))
 
     answers = {}
-    for index, (q1, q2, q3) in enumerate(question_pairs):
+    for index, (q1, q3, q4) in enumerate(question_pairs):
         prompt_template = PromptTemplate(
             input_variables=["q1", "q2", "q3"],
             template=prompt
         )
-        llms = PromptLLMS(llm_model,prompt_template, q1, q2, q3)
+        llms = PromptLLMS(llm_model,prompt_template, q1, q3, q4)
         llm_response = llms.execute_three_question()
         if language == 'en':
-            real_relation = real_relations_en[input_filename]
             output_prefix = ''
         else:
-            real_relation = real_relations_es[input_filename]
             output_prefix = '*'
         converted_response = utils.convert_response_to_set_class(llm_response, real_relation)
 
         answers[index] = converted_response
 
-        print(f"Question {index + 1}: {q1} | {q2} | {q3}")
+        print(f"Question {index + 1}: {q1} | {q3} | {q4}")
         print(f"LLM Response: {llm_response}")
-    output_filename = os.path.join(here, f'../data/answers/zero-shot/relation-classification/{output_prefix}Minus_{llm_model}.json')
+    output_filename = os.path.join(here, f'../data/answers/zero-shot/{dataset.split('.')[0]}/relation-classification/{output_prefix}Minus_{llm_model}.json')
     
     with open(output_filename, 'w', encoding='utf-8') as f:
         json.dump(answers, f, ensure_ascii=False, indent=4)
 
-def equivalence_test(llm_model, language):
-    input_filename = 'equal-wiki.tsv'
-    tsv_file = utils.get_dataset_path(input_filename, language)
+def equivalence_test(llm_model, language, dataset, real_relation):
+    tsv_file = utils.get_dataset_path(dataset, language)
     prompt = PROMPTS[language]
 
     # Load questions
@@ -154,7 +145,7 @@ def equivalence_test(llm_model, language):
     with open(tsv_file, newline='', encoding='utf-8') as tsvfile:
         reader = csv.DictReader(tsvfile, delimiter='\t')
         for row in reader:
-            question_pairs.append((row['ql1'], row['ql2']))
+            question_pairs.append((row['Q1'], row['Q2']))
 
     answers = {}
 
@@ -166,10 +157,8 @@ def equivalence_test(llm_model, language):
         llms = PromptLLMS(llm_model, prompt_template, q1, q2)
         llm_response = llms.execute_two_question()
         if language == 'en':
-            real_relation = real_relations_en[input_filename]
             output_prefix = ''
         else:
-            real_relation = real_relations_es[input_filename]
             output_prefix = '*'
         converted_response = utils.convert_response_to_set_class(llm_response, real_relation)
 
@@ -177,32 +166,30 @@ def equivalence_test(llm_model, language):
 
         print(f"Question {index + 1}: {q1} | {q2}")
         print(f"LLM Response: {llm_response}")
-    output_filename = os.path.join(here, f'../data/answers/zero-shot/relation-classification/{output_prefix}Equivalence_{llm_model}.json')
-    
+        
+    output_filename = os.path.join(here, f'../data/answers/zero-shot/{dataset.split('.')[0]}/relation-classification/{output_prefix}Equivalence_{llm_model}.json')
     with open(output_filename, 'w', encoding='utf-8') as f:
         json.dump(answers, f, ensure_ascii=False, indent=4)
 
-def sup_sub_test(llm_model, language):
+def sup_sub_test(llm_model, language, dataset, real_relation = 'Containment'):
     chat = prompt_llms.return_chat_model(llm_model)
-    input_filename = 'subsetOf-wiki.tsv'
-    tsv_file = utils.get_dataset_path(input_filename, language)
-    prompt = PROMPTS[language]
+    tsv_file = utils.get_dataset_path(dataset, language)
 
     # Load questions
     question_pairs = []
     with open(tsv_file, newline='', encoding='utf-8') as tsvfile:
         reader = csv.DictReader(tsvfile, delimiter='\t')
         for row in reader:
-            question_pairs.append((row['ql1'], row['ql2']))
+            question_pairs.append((row['Q1'], row['Q3']))
 
     answers = {}
-    for index, (q1, q2) in enumerate(question_pairs):
+    for index, (q1, q3) in enumerate(question_pairs):
         memory = ConversationBufferMemory()
         conversation = ConversationChain(
             llm=chat,
             memory=memory
         )
-        relation_predicted = conversation.predict(input=PROMPTS[language].format(q1=q2, q2=q1))
+        relation_predicted = conversation.predict(input=PROMPTS[language].format(q1=q1, q2=q3))
         relation_predicted = relation_predicted.strip().lower()
         print(f"Relation predicted: {relation_predicted}")
         if relation_predicted == 'containment' or relation_predicted == 'contención':
@@ -218,30 +205,30 @@ def sup_sub_test(llm_model, language):
             output_prefix = '*'
 
         answers[index] = converted_response
-        
-        print(f"Question {index + 1}: q1: {q2} | q2: {q1}")
+
+        print(f"Question {index + 1}: q1: {q1} | q2: {q3}")
         print(f"LLM relation: {relation_predicted}, LLM direction: {direction.strip().lower()}")
-        
-    output_filename = os.path.join(here, f'../data/answers/zero-shot/relation-classification/{output_prefix}Containment_{llm_model}.json')
-    
+
+    output_filename = os.path.join(here, f'../data/answers/zero-shot/{dataset.split('.')[0]}/relation-classification/{output_prefix}Containment_{llm_model}.json')
+
     with open(output_filename, 'w', encoding='utf-8') as f:
         json.dump(answers, f, ensure_ascii=False, indent=4)
+
+dataset = 'qawiki.tsv'
 
 for language in languages:
     for llm_model in llm_models:
         # Run equivalence test
         print(f"Processing model: {llm_model} for language: {language}")
-        equivalence_test(llm_model, language)
+        #equivalence_test(llm_model, language, dataset, 'Equivalence')
         print(f"Finished processing model: {llm_model} for language: {language})\n")
 
         # Run Containment test
         print(f"Processing model: {llm_model} for language: {language}")
-        sup_sub_test(llm_model, language)
+        #sup_sub_test(llm_model, language, dataset, 'Containment')
         print(f"Finished processing model: {llm_model} for language: {language}\n")
 
         # Run minus test
         print(f"Processing model: {llm_model} for language: {language}")
-        minus_test(llm_model, language)
+        minus_test(llm_model, language, dataset, 'Minus')
         print(f"Finished processing model: {llm_model} for language: {language}\n")
-
-        break
