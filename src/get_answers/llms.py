47eb40469031182a92e4e9e14c4bd9dfa2d78dc2
Michealxh-L <liuxianhaoMicheal@gmail.com>
2025-09-02 16:50:42 +0200

    update answers from gpt-5

--- src/get_answers/llms.py
+++ src/get_answers/llms.py
@@ -27,9 +27,10 @@ azure_openai_key = os.getenv('AZURE_OPENAI_API_KEY')
 azure_endpoint = os.getenv('AZURE_OPENAI_ENDPOINT')
 azure_api_version= os.getenv('AZURE_API_VERSION', '2024-12-01-preview')
 
-azure_models = ["gpt-4o","o3","o1","gpt-5-mini","gpt-5"]
+# azure_models = ["gpt-4o","o3","o1","gpt-5-mini","gpt-5"]
+azure_models = []
 openai_models_notemperature = ["o3","gpt-5-nano"]
-openai_models = ['gpt-5',"gpt-5-mini",'gpt-4.1-nano-2025-04-14', 'gpt-4.1-mini-2025-04-14', 'gpt-4.1-2025-04-14',"gpt-4o","gpt-4.1"]
+openai_models = ['gpt-5',"gpt-5-mini","gpt-5-nano",'gpt-4.1-nano-2025-04-14', 'gpt-4.1-mini-2025-04-14', 'gpt-4.1-2025-04-14',"gpt-4o","gpt-4.1"]
 gemini_models = ["gemini-2.0-flash","gemini-2.5-pro","gemini-2.5-flash"]
 xai_models = ['grok-3-mini','grok-4-0709']
 claude_models = ['claude-3-5-sonnet-20240620','claude-3-haiku']
@@ -102,7 +103,7 @@ def return_chat_model(model_name, temperature=0, max_tokens = 20000):
     if model_name in azure_models:
         return AzureChatOpenAI(azure_deployment=model_name, api_version=azure_api_version,)
     elif model_name in openai_models:
-        return ChatOpenAI(model=model_name, openai_api_key=openai_api_key, temperature=temperature)
+        return ChatOpenAI(model=model_name, openai_api_key=openai_api_key)
     elif model_name in openai_models_notemperature:
         return ChatOpenAI(model=model_name, openai_api_key=openai_api_key, max_tokens=max_tokens)
     elif model_name in gemini_models:
